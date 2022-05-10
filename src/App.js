3e7f0c83401c66dcc169c856c7d1af81038f3405
Kenya Guzman <kenya.guzman@inviu.com.ar>
2022-05-10 17:49:09 -0300

    Add charting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
+import { createChart } from 'lightweight-charts';
+
+const chart = createChart(document.body, { width: window.width, height: 300 });
+const lineSeries = chart.addLineSeries();
+lineSeries.setData([
+    { time: '2019-04-11', value: 80.01 },
+    { time: '2019-04-12', value: 96.63 },
+    { time: '2019-04-13', value: 76.64 },
+    { time: '2019-04-14', value: 81.89 },
+    { time: '2019-04-15', value: 74.43 },
+    { time: '2019-04-16', value: 80.01 },
+    { time: '2019-04-17', value: 96.63 },
+    { time: '2019-04-18', value: 76.64 },
+    { time: '2019-04-19', value: 81.89 },
+    { time: '2019-04-20', value: 74.43 },
+]);
+
+const options = {
+  alignLabels: true,
+  timeScale: {
+    rightOffset: 12,
+    barSpacing: 3,
+    fixLeftEdge: true,
+    lockVisibleTimeRangeOnResize: true,
+    rightBarStaysOnScroll: true,
+    borderVisible: false,
+    borderColor: "#fff000",
+    visible: true,
+    timeVisible: true,
+    secondsVisible: false
+  }
+};
+
+const candlestickSeries = [{
+  data: [
+    { time: '2018-10-19', open: 180.34, high: 180.99, low: 178.57, close: 179.85 },
+    { time: '2018-10-22', open: 180.82, high: 181.40, low: 177.56, close: 178.75 },
+    { time: '2018-10-23', open: 175.77, high: 179.49, low: 175.44, close: 178.53 },
+    { time: '2018-10-24', open: 178.58, high: 182.37, low: 176.31, close: 176.97 },
+    { time: '2018-10-25', open: 177.52, high: 180.50, low: 176.83, close: 179.07 },
+    { time: '2018-10-26', open: 176.88, high: 177.34, low: 170.91, close: 172.23 },
+    { time: '2018-10-29', open: 173.74, high: 175.99, low: 170.95, close: 173.20 },
+    { time: '2018-10-30', open: 173.16, high: 176.43, low: 172.64, close: 176.24 },
+    { time: '2018-10-31', open: 177.98, high: 178.85, low: 175.59, close: 175.88 },
+    { time: '2018-11-01', open: 176.84, high: 180.86, low: 175.90, close: 180.46 },
+    { time: '2018-11-02', open: 182.47, high: 183.01, low: 177.39, close: 179.93 },
+    { time: '2018-11-05', open: 181.02, high: 182.41, low: 179.30, close: 182.19 }
+  ]
+}]
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +65,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>Charting</h1>
+      <div id="chart-container"></div>
     </div>
   );
 }
